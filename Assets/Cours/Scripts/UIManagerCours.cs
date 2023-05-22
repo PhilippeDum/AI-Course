c1168d306c211d090b7bcc98f26eac825831bf44
PhilippeDum <phil.dum88@gmail.com>
2023-05-22 15:56:32 +0200

    [UPDATE] Update UI Cours part

---
+++ Assets/Cours/Scripts/UIManagerCours.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerCours : MonoBehaviour
+{
+    [SerializeField] private TMP_Text tiberium;
+    [SerializeField] private Slider energy;
+    [SerializeField] private Slider energyUsed;
+
+    private void Awake()
+    {
+        UpdateTiberium(0);
+        UpdateMaxEnergy(100);
+        UpdateEnergyUI(25);
+        UpdateEnergyUsedUI(0);
+    }
+
+    public void UpdateTiberium(int value)
+    {
+        tiberium.text = $"{value}";
+    }
+
+    public void UpdateMaxEnergy(int value)
+    {
+        energy.maxValue = value;
+        energyUsed.maxValue = value;
+    }
+
+    public void UpdateEnergyUI(int value)
+    {
+        energy.value = value;
+    }
+
+    public void UpdateEnergyUsedUI(int value)
+    {
+        energyUsed.value = value;
+    }
+}
