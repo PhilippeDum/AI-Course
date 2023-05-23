8fd5cce7b8ddc94718b59a9757293fd5aaa6c08c
PhilippeDum <phil.dum88@gmail.com>
2023-05-23 12:00:46 +0200

    [FIX | UPDATE | ADD] Fix "Stats -> UnitStats" script missing + Update Blueprint placement + Add new King Tower Model & Textures

--- Assets/Cours/Scripts/Gameplay/BlueprintController.cs
+++ Assets/Cours/Scripts/Gameplay/BlueprintController.cs
@@ -2,25 +2,11 @@ using UnityEngine;
 
 public class BlueprintController : MonoBehaviour
 {
-    [SerializeField] private Material invalidMaterial;
-
-    private int layerMaskTerrain = 1 << 9;
     private int nbCollision = 0;
-    private bool hasValidPlacement = true;
-    private Material defaultMaterial;
-
-    private void Start()
-    {
-        defaultMaterial = transform.GetChild(0).GetComponent<Renderer>().sharedMaterial;
-    }
 
     private void Update()
     {
-        FollowCursor();
-
         CheckValidPlacement();
-
-        SetMaterial();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,40 +27,15 @@ public class BlueprintController : MonoBehaviour
 
     #region Custom Methods
 
-    private void FollowCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 150, layerMaskTerrain))
-        {
-            transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            //transform.position = hit.point;
-        }
-    }
-
-    private void CheckValidPlacement()
+    public bool CheckValidPlacement()
     {
         if (nbCollision > 0)
         {
-            hasValidPlacement = false;
+            return false;
         }
         else
         {
-            hasValidPlacement = true;
-        }
-    }
-
-    private void SetMaterial()
-    {
-        if (hasValidPlacement)
-        {
-            transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = defaultMaterial;
-        }
-        else
-        {
-            transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = invalidMaterial;
+            return true;
         }
     }
 
--- Assets/Cours/Scripts/Managers/BlueprintManager.cs
+++ Assets/Cours/Scripts/Managers/BlueprintManager.cs
@@ -2,10 +2,89 @@ using UnityEngine;
 
 public class BlueprintManager : MonoBehaviour
 {
-    [SerializeField] private GameObject generator;
+    [SerializeField] private GameObject building;
+    [SerializeField] private Material invalidMaterial;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private GameObject blueprintGO;
+    private BlueprintController blueprint;
+    private Material defaultMaterial;
+    private int layerMaskTerrain = 1 << 9;
+
+    private void Update()
+    {
+        if (blueprintGO)
+        {
+            SetMaterial();
+
+            CursorControls();
+        }
+    }
 
     public void InstantiateBuilding()
     {
-        Instantiate(generator, Vector3.zero, Quaternion.identity);
+        blueprintGO = Instantiate(building, Vector3.zero, Quaternion.identity);
+        blueprint = blueprintGO.GetComponent<BlueprintController>();
+        defaultMaterial = blueprintGO.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial;
+    }
+
+    private void CursorControls()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 150, layerMaskTerrain))
+        {
+            //blueprintGO.transform.position = new Vector3(hit.point.x, blueprintGO.transform.position.y, hit.point.z);
+            blueprintGO.transform.position = hit.point;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            blueprintGO.transform.Rotate(Vector3.up * rotationSpeed, Space.Self);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            blueprintGO.transform.Rotate(-Vector3.up * rotationSpeed, Space.Self);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (blueprint.CheckValidPlacement()) PlaceBlueprint();
+        }
+    }
+
+    private void PlaceBlueprint()
+    {
+        blueprintGO = null;
+
+        Destroy(blueprint);
+    }
+
+    private void SetMaterial()
+    {
+        if (blueprint.CheckValidPlacement())
+        {
+            Renderer[] chidlRenderers = blueprintGO.transform.GetChild(0).GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer renderer in chidlRenderers)
+            {
+                renderer.sharedMaterial = defaultMaterial;
+            }
+
+            //transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = defaultMaterial;
+        }
+        else
+        {
+            Renderer[] chidlRenderers = blueprintGO.transform.GetChild(0).GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer renderer in chidlRenderers)
+            {
+                renderer.sharedMaterial = invalidMaterial;
+            }
+
+            //transform.GetChild(0).GetComponent<Renderer>().sharedMaterial = invalidMaterial;
+        }
     }
 }
